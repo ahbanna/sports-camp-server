3f8f372e93cf4f3313a775e426077ecb311fd67a
Al Hasanul Banna <tutul5800@gmail.com>
2023-06-14 19:31:23 +0600

    payment integrated

--- index.js
+++ index.js
@@ -1,144 +1,17 @@
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // MongoDB starts
-
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k7baavr.mongodb.net/?retryWrites=true&w=majority`;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // collection starts
-//     const InstructorCollection = client
-//       .db("sportsCamp")
-//       .collection("Instructors");
-//     const userCollection = client.db("sportsCamp").collection("users");
-
-//     // collection end
-
-//     // Instructors RAED starts
-//     app.get("/Instructors", async (req, res) => {
-//       const cursor = InstructorCollection.find();
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     // Instructors RAED end
-
-//     // user CREATE api to receive data from client side starts
-//     app.post("/users", async (req, res) => {
-//       const user = req.body;
-//       // for google signup it check is the user already registered or noet starts
-//       const query = { email: user.email };
-//       const existingUser = await userCollection.findOne(query);
-//       console.log("Existing user:", existingUser);
-//       if (existingUser) {
-//         return res.send({ message: "This user is already exists " });
-//       }
-//       // for google end
-//       const result = await userCollection.insertOne(user);
-//       res.send(result);
-//     });
-//     // user CREATE api to receive data from client side end
-
-//     // user READ starts
-//     app.get("/users", async (req, res) => {
-//       const result = await userCollection.find().toArray();
-//       res.send(result);
-//     });
-//     // user READ end
-
-//     // make UPDATE user as admin starts with PH
-//     // app.patch("/users/admin/:id", async (req, res) => {
-//     //   const id = req.params.id;
-//     //   const filter = { _id: new ObjectId(id) };
-//     //   const updateDoc = {
-//     //     $set: {
-//     //       role: "admin",
-//     //     },
-//     //   };
-//     //   const result = await userCollection.updateOne(filter, updateDoc);
-//     //   res.send(result);
-//     // });
-//     // make UPDATE user as admin end
-
-//     // make UPDATE user role starts
-//     app.patch("/users/:role/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const role = req.params.role;
-//       const filter = { _id: new ObjectId(id) };
-//       const updateDoc = {
-//         $set: {
-//           role: role,
-//         },
-//       };
-//       const result = await userCollection.updateOne(filter, updateDoc);
-//       res.send(result);
-//     });
-//     // make UPDATE user role ends
-
-//     // // Set default role for a user
-//     // app.patch("/users/default-role/:id", async (req, res) => {
-//     //   const id = req.params.id;
-//     //   const filter = { _id: new ObjectId(id) };
-//     //   const updateDoc = {
-//     //     $set: {
-//     //       role: "student", // Set the default role as "student"
-//     //     },
-//     //   };
-//     //   const result = await userCollection.updateOne(filter, updateDoc);
-//     //   res.send(result);
-//     // });
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// // MongoDB end
-
-// app.get("/", (req, res) => {
-//   res.send("Summer camp server is running");
-// });
-// app.listen(port, () => {
-//   console.log(`Summer camp server is running on port:${port}`);
-// });
-
-// THE ABOVE CODE BEFORE ROUTE SECURE
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const app = express();
+// payment
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+// const stripe = require("stripe")(
+//   "sk_test_51NI6rLBANJNml86HWIBD25esKCdrwUkEdA57r2WbK7lAvyrXb58FRlMha6tIb3rmgYmibNVtnZ1PWTgf7dD3YIJD00bX7N2qQe"
+// );
 const port = process.env.PORT || 5000;
 
 // middleware
+
 app.use(cors());
 app.use(express.json());
 
@@ -152,22 +25,26 @@ const verifyJWT = (req, res, next) => {
   }
   //bearer token
   const token = authorization.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res
-        .status(401)
-        .send({ error: true, message: "Unauthorized access" });
+  jwt.verify(
+    token,
+    "76fbbca640e8e562c4b9ab21b45fccf86c8b98da707e92d4b58e3e3e5b975f4ee6c2c8bd8cbaf52ae191e280bed82880c88698f6271f5e7627c85a022f2e6b84",
+    (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ error: true, message: "Unauthorized access" });
+      }
+      req.decoded = decoded;
+      next();
     }
-    req.decoded = decoded;
-    next();
-  });
+  );
 };
 // verify jwt end
 
 // MongoDB starts
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k7baavr.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://sportsCampUser:5l3Ydew9UAMYTAQ9@cluster0.k7baavr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -181,7 +58,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // collection starts
     const InstructorCollection = client
       .db("sportsCamp")
@@ -191,7 +68,43 @@ async function run() {
     const selectedClassCollection = client
       .db("sportsCamp")
       .collection("selectedclasses");
+    const paymentCollection = client.db("sportsCamp").collection("payments");
     // collection end
+
+    // create payment intent starts
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseFloat(price) * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+    // payment intent end
+
+    // payment api starts
+    app.post("/payments", verifyJWT, async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+
+      res.send(result);
+    });
+    // payment api end
+
+    // payment READ starts
+    app.get("/payments", async (req, res) => {
+      let query = {};
+      if (req.query?.userEmail) {
+        query = { userEmail: req.query.userEmail };
+      }
+      const cursor = paymentCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // payment READ end
 
     // gererate jwt token starts
     app.post("/jwt", (req, res) => {
@@ -233,20 +146,6 @@ async function run() {
       res.send(result);
     });
     // user READ end
-
-    // make UPDATE user as admin starts with PH
-    // app.patch("/users/admin/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: {
-    //       role: "admin",
-    //     },
-    //   };
-    //   const result = await userCollection.updateOne(filter, updateDoc);
-    //   res.send(result);
-    // });
-    // make UPDATE user as admin end
 
     // check if the user is admin or not starts
     app.get("/users/admin/:email", verifyJWT, async (req, res) => {
@@ -292,20 +191,6 @@ async function run() {
       res.send(result);
     });
     // make UPDATE user role ends
-
-    // // Set default role for a user start
-    // app.patch("/users/default-role/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: {
-    //       role: "student", // Set the default role as "student"
-    //     },
-    //   };
-    //   const result = await userCollection.updateOne(filter, updateDoc);
-    //   res.send(result);
-    // });
-    // // Set default role for a user start
 
     // class CREATE api to receive data from client side starts
     app.post("/allclasses", async (req, res) => {
@@ -382,21 +267,6 @@ async function run() {
       res.send(result);
     });
     // make approve or denay class ends
-
-    // Experiment for check for is it admin starts
-    // app.get("/users/admin", verifyJWT, async (req, res) => {
-    //   const email = req.params.email;
-
-    //   if (req.decoded.email !== email) {
-    //     res.send({ admin: false });
-    //   }
-
-    //   const query = { email: email };
-    //   const user = await userCollection.findOne(query);
-    //   const result = { admin: user?.role === "admin" };
-    //   res.send(result);
-    // });
-    // Experiment for check for is it admin end
 
     // selected class CREATE api to receive data from client side starts
     app.post("/selectedclasses", async (req, res) => {
